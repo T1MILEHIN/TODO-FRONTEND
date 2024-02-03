55c563c6ec9b0286b5424db968031983b06c6eca
Timi <tunmisebabade@gmail.com>
2024-02-03 17:04:11 +0100

    first commit

--- src/api/TodosApi.jsx
+++ src/api/TodosApi.jsx
@@ -4,25 +4,25 @@ import axios from "axios";
 // })
 
 export const FetchAllTodos = async () => {
-    const response = await axios.get("/api/")
+    const response = await axios.get("https://todo-fsuw.onrender.com/api/")
     return response.data
 }
 
 export const CreateNewToDo =  async (newToDo) => {
-    return await axios.post("/api/create/", newToDo);
+    return await axios.post("https://todo-fsuw.onrender.com/api/create/", newToDo);
 }
 
 export const SingleTodoApi = async (ID)=> {
-    const response = await axios.get(`/api/${ID}/`)
+    const response = await axios.get(`https://todo-fsuw.onrender.com/api/${ID}/`)
     return response.data
 }
 
 export const UpdateTodoApi = async (data, id)=> {
-    return axios.put(`/api/update/${id}/`, data)
+    return axios.put(`https://todo-fsuw.onrender.com/api/update/${id}/`, data)
 }
 
 export const DeleteTodoApi = async (id)=> {
-    return axios.delete(`/api/delete/${id}`)
+    return axios.delete(`https://todo-fsuw.onrender.com/api/delete/${id}`)
 }
 
 // export default TodosApi;
